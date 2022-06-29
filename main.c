3534d2de2af52937218e367d85d5096fba7e3954
ArtemLeshch <artemleshchukov02@gmail.com>
2022-06-29 20:32:00 +0400

    made 1-7 exercise

--- main.c
+++ main.c
@@ -1,44 +1,17 @@
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 0
-#define STEP 0
-
-// Fahrenheit-Celsius table
-void fahrCelsTempConversion()
+void charInputOutput()
 {
-	printf("Fahrenheit to Celsius Conversion Table\n");
-	printf("--------------------------------------\n");
+	int c;
 	
-	for (int fahr = UPPER; fahr >= LOWER; fahr -= STEP)
-		printf("%3d %6.1f\n", fahr, (5.0 / 9) * (fahr - 32));
-}
-
-// Celsius-Fahrenheit table
-void celsFahrTempConversion()
-{
-	printf("Celsius to Fahrenheit Conversion Table\n");
-	printf("--------------------------------------\n");
-	float fahr, cels;
-	int lower, upper, step;
-	
-	lower = -60;
-	upper = 200;
-	step = 20;
-	
-	cels = lower;
-	while (cels <= upper)
-	{
-		fahr = (9.0 / 5) * cels + 32;
-		printf("%3.0f %6.1f\n", cels, fahr);
-		cels += step;
-	}
+	while ((c = getchar()) != EOF)
+		putchar(c);
+	putchar(c);
 }
 
 int main()
 {
-	fahrCelsTempConversion();
-	celsFahrTempConversion();
+	charInputOutput();
 	return 0;
 }
 
